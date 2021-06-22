000607000f6db344da47ce4b70b70f2c4c0a1d75
kbranavitski <kbranavitski@ibagroup.eu>
2021-06-22 16:14:25 +0300

    release 0.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ apply(plugin = "kotlin")
 apply(plugin = "org.jetbrains.intellij")
 
 group = "eu.ibagroup"
-version = "0.4"
+version = "0.4.1"
 
 repositories {
   mavenCentral()
@@ -64,13 +64,14 @@ tasks.getByName<PatchPluginXmlTask>("patchPluginXml") {
   untilBuild("211.*")
   changeNotes(
     """
-      In version 0.4 we added:<br/>
+      In version 0.4.1 we added:<br/>
       <ul>
         <li>Pride logo to support LGBTQIA+ community. Peace, love, pride</li>
         <li>Job submission by the right click on files in the File Explorer</li>
         <li>Move and Copy operations are available for USS files and directories</li>
         <li>Editing Working Sets is now accessible by the right click on the Working Set in the File Explorer</li>
         <li>Tracking analytics events is now enabled with corresponding Privacy Policy</li>
+        <li>Small UI fixes.</li>
       </ul>"""
   )
 }
