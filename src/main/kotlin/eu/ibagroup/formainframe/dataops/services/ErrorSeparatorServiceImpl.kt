70ab9354d95bbdf2490c03a10d3739c891d85660
Valiantsin Krus <76202482+hunts396@users.noreply.github.com>
2021-08-30 15:03:08 +0300

    IJMP-226: hot fix substring error.

--- src/main/kotlin/eu/ibagroup/formainframe/dataops/services/ErrorSeparatorServiceImpl.kt
+++ src/main/kotlin/eu/ibagroup/formainframe/dataops/services/ErrorSeparatorServiceImpl.kt
@@ -4,18 +4,23 @@ import java.util.*
 
 class ErrorSeparatorServiceImpl : ErrorSeparatorService {
   override fun separateErrorMessage(errorMessage: String): Properties {
-    val indOfErrorCodeDelimiter = errorMessage.indexOf(' ')
-    val indOfErrorPostfix = errorMessage.lastIndexOf('(')
-
-    val errorCode = errorMessage.subSequence(0, indOfErrorCodeDelimiter)
-    val description = errorMessage.subSequence(indOfErrorCodeDelimiter + 1, indOfErrorPostfix-1);
-    val errorPostfix = errorMessage.subSequence(indOfErrorPostfix, errorMessage.length)
-
     val errorProperties = Properties()
-    errorProperties["error.code"] = errorCode
-    errorProperties["error.description"] = description
-    errorProperties["error.postfix"] = errorPostfix
+    try {
+      val indOfErrorCodeDelimiter = errorMessage.indexOf(' ')
+      val indOfErrorPostfix = errorMessage.lastIndexOf('(')
 
+      val errorCode = errorMessage.subSequence(0, indOfErrorCodeDelimiter)
+      val description = errorMessage.subSequence(indOfErrorCodeDelimiter + 1, indOfErrorPostfix-1);
+      val errorPostfix = errorMessage.subSequence(indOfErrorPostfix, errorMessage.length)
+
+      errorProperties["error.code"] = errorCode
+      errorProperties["error.description"] = description
+      errorProperties["error.postfix"] = errorPostfix
+
+      return errorProperties
+    } catch (e: Exception) {
+      errorProperties["error.description"] = errorMessage
+    }
     return errorProperties
   }
 }
