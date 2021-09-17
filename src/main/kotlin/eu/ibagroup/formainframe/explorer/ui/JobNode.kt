3ea7e0c077451677f9c5129afd22443d75bc4860
Valiantsin Krus <76202482+hunts396@users.noreply.github.com>
2021-09-17 21:21:39 +0300

    IJMP-295: job nodes ui reworked, fixed bug of invalid saving jobs to vfs.

--- src/main/kotlin/eu/ibagroup/formainframe/explorer/ui/JobNode.kt
+++ src/main/kotlin/eu/ibagroup/formainframe/explorer/ui/JobNode.kt
@@ -4,19 +4,25 @@
 
 package eu.ibagroup.formainframe.explorer.ui
 
+import com.intellij.icons.AllIcons
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.util.treeView.AbstractTreeNode
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.ui.SimpleTextAttributes
+import eu.ibagroup.formainframe.dataops.DataOpsManager
 import eu.ibagroup.formainframe.dataops.RemoteQuery
 import eu.ibagroup.formainframe.dataops.UnitRemoteQueryImpl
+import eu.ibagroup.formainframe.dataops.attributes.RemoteJobAttributes
 import eu.ibagroup.formainframe.dataops.fetch.JobQuery
 import eu.ibagroup.formainframe.dataops.fetch.LibraryQuery
 import eu.ibagroup.formainframe.explorer.FilesWorkingSet
 import eu.ibagroup.formainframe.explorer.JesFilterUnit
 import eu.ibagroup.formainframe.explorer.JesWorkingSet
 import eu.ibagroup.formainframe.vfs.MFVirtualFile
+import icons.ForMainframeIcons
 
+private val jobIcon = AllIcons.Nodes.Folder
 
 class JobNode(
   library: MFVirtualFile,
@@ -48,6 +54,11 @@ class JobNode(
   override val requestClass = JobQuery::class.java
 
   override fun update(presentation: PresentationData) {
-    presentation.addText(value.name, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+    val attributes = service<DataOpsManager>().tryToGetAttributes(value) as? RemoteJobAttributes
+    val job = attributes?.jobInfo
+    val jobIdText = if (job == null) "" else "(${job.jobId})"
+    presentation.addText("${job?.jobName ?: ""} ${jobIdText} ", SimpleTextAttributes.REGULAR_ATTRIBUTES)
+    presentation.addText(job?.status?.value ?: "", SimpleTextAttributes.GRAYED_ATTRIBUTES)
+    presentation.setIcon(jobIcon)
   }
 }
--- src/main/kotlin/eu/ibagroup/formainframe/explorer/ui/SpoolFileNode.kt
+++ src/main/kotlin/eu/ibagroup/formainframe/explorer/ui/SpoolFileNode.kt
@@ -1,11 +1,17 @@
 package eu.ibagroup.formainframe.explorer.ui
 
+import com.intellij.icons.AllIcons
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.util.treeView.AbstractTreeNode
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.ui.SimpleTextAttributes
+import eu.ibagroup.formainframe.dataops.DataOpsManager
+import eu.ibagroup.formainframe.dataops.attributes.RemoteSpoolFileAttributes
 import eu.ibagroup.formainframe.explorer.ExplorerUnit
 import eu.ibagroup.formainframe.vfs.MFVirtualFile
+
+private val spoolFileIcon = AllIcons.FileTypes.Text
 
 class SpoolFileNode(
   file: MFVirtualFile,
@@ -17,7 +23,11 @@ class SpoolFileNode(
   file, project, parent, unit, treeStructure
 ), MFNode {
   override fun update(presentation: PresentationData) {
-    presentation.addText(value.name, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+    val attributes = service<DataOpsManager>().tryToGetAttributes(value) as? RemoteSpoolFileAttributes
+    val spoolFile = attributes?.info
+    presentation.setIcon(spoolFileIcon)
+    presentation.addText("${value.name} ", SimpleTextAttributes.REGULAR_ATTRIBUTES)
+    presentation.addText(spoolFile?.procStep ?: "", SimpleTextAttributes.GRAYED_ATTRIBUTES)
   }
 
   override fun getChildren(): MutableCollection<out AbstractTreeNode<*>> {
