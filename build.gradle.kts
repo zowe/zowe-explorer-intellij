1630f439daf100c9e25edae18f85a0d0f109cf55
Valiantsin Krus <76202482+hunts396@users.noreply.github.com>
2022-01-17 17:30:17 +0300

    IJMP-335: changed zowe-r2z version

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
   implementation("org.jgrapht:jgrapht-core:1.5.0")
   implementation("com.starxg:java-keytar:1.0.0")
-  implementation("eu.ibagroup:zowe-r2z:1.0.23")
+  implementation("eu.ibagroup:zowe-r2z:1.0.24")
   implementation("com.segment.analytics.java:analytics:+")
   testImplementation("junit", "junit", "4.12")
 }
