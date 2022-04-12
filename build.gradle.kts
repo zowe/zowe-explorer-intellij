ebf7d9b527c31b5b82d141fd4935252ca74f171e
Valiantsin Krus <76202482+hunts396@users.noreply.github.com>
2022-04-12 16:43:48 +0300

    IJMP-482: fixed download for binary files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.30")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
   implementation("org.jgrapht:jgrapht-core:1.5.0")
-  implementation("eu.ibagroup:r2z:1.0.20")
+  implementation("eu.ibagroup:r2z:1.0.22")
   implementation("com.segment.analytics.java:analytics:+")
   testImplementation("junit", "junit", "4.12")
 }
