d7daee35b17dab59fbb521f3f6540e5dc6d24c1a
Uladzislau Kalesnikau <Uladzislau.Kalesnikau1@ibm.com>
2022-04-15 10:34:53 +0300

    Release v0.6.0 preparation

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ apply(plugin = "kotlin")
 apply(plugin = "org.jetbrains.intellij")
 
 group = "eu.ibagroup"
-version = "0.5.2"
+version = "0.6.0"
 
 repositories {
   mavenCentral()
@@ -64,30 +64,54 @@ dependencies {
 }
 
 intellij {
-  version = "2021.3"
+  version = "2022.1"
 }
 
 tasks.getByName<PatchPluginXmlTask>("patchPluginXml") {
   sinceBuild("203.5981")
-  untilBuild("213.*")
+  untilBuild("221.*")
   changeNotes(
     """
       <b>Expanded changes list:</b><br/>
       <ul>
-        <li>Manual sync option implemented</li>
-        <li>Separate icons for Datasets and USS folders</li>
-        <li>Autosave reworks</li>
-        <li>Logs in jobs console log view</li>
-        <li>Parse config xml file in old format</li>
+        <li>Manual sync option</li>
+        <li>Delete dataset/file when it is being edited</li>
+        <li>Job Console log view</li>
+        <li>Copy from remote to local</li>
+        <li>Job operations</li>
+        <li>Separate icons for Datasets</li>
+        <li>Autosync reworked</li>
+        <li>User configs parsing in new format</li>
+        <li>Force rename</li>
+        <li>Settings tab</li>
       </ul>
       <b>Bugs fixed:</b>
       <ul>
-        <li>IDE Fatal errors if delete the connection that has working set</li>
-        <li>Delete option does not work for Working Sets in File Explorer</li>
-        <li>Right click->New->WorkingSet on JES Explorer page opens dialog to create WorkingSet instead of JobsWorkingSet</li>
-        <li>Creating z/Os mask with first digit in HLQ is not blocked</li>
-        <li>The creation of masks with three or more asterisks is not blocked</li>
         <li>Renaming the mask in the file explorer to existing one does not return any message</li>
+        <li>The creation of masks with three or more asterisks is not blocked</li>
+        <li>Error when the prefix length for JobFilter is more than 8 characters or Prefix contains unacceptable symbols</li>
+        <li>NPE when notification was closed</li>
+        <li>Change lower case to upper case during DS mask creation</li>
+        <li>Wrong Directory allocation restrictions</li>
+        <li>Error 404 during dataset creation</li>
+        <li>Block Size is being overwritten on error</li>
+        <li>Missing UI error message for z/Os mask with length more than 44 chars</li>
+        <li>Adding job filter through context menu item in JES Explorer doesn't work correctly</li>
+        <li>Creating z/Os mask with first digit in HLQ is not blocked</li>
+        <li>Right click->New->WorkingSet on JES Explorer page opens dialog to create WorkingSet instead of JobsWorkingSet</li>
+        <li>Delete option does not work for Working Sets in File Explorer.</li>
+        <li>Hotfix. Fix plugin compatibility with old versions of Inellij platform.</li>
+        <li>ScrollPanel in "Allocate Dataset" frame</li>
+        <li>Renaming an uss folder/file and then renaming it back to its original name causes an error</li>
+        <li>No message details when renaming dataset to existing name</li>
+        <li>Error message for failed dataset allocation should be duplicated in Dataset Allocation window</li>
+        <li>Impossible to close job's output</li>
+        <li>IDE Fatal errors if delete the connection that has working set</li>
+        <li>Use binary mode changes file contents</li>
+        <li>Bad connection was deleted</li>
+        <li>NullPointerException when tree opens by user with incorrect password.</li>
+        <li>Username/Password are not validated during connection creation </li>
+        <li>Impossible to close the file located locally on PC (the file was opened not from plugin)</li>
       </ul>"""
   )
 
