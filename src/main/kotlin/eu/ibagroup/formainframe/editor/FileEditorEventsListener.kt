fe93c9ae8f341342a6dce5efa613628c591bb08f
Denis Lisenkov <DLisiankou@ibagroup.eu>
2023-03-10 11:12:49 +0300

    IJMP-961: autosync works now

--- src/main/kotlin/eu/ibagroup/formainframe/editor/FileEditorEventsListener.kt
+++ src/main/kotlin/eu/ibagroup/formainframe/editor/FileEditorEventsListener.kt
@@ -11,7 +11,6 @@
 package eu.ibagroup.formainframe.editor
 
 import com.intellij.openapi.components.service
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.progress.runModalTask
@@ -21,8 +20,6 @@ import eu.ibagroup.formainframe.dataops.DataOpsManager
 import eu.ibagroup.formainframe.dataops.content.synchronizer.*
 import eu.ibagroup.formainframe.utils.*
 import eu.ibagroup.formainframe.vfs.MFVirtualFile
-
-private val log = log<FileEditorEventsListener>()
 
 /**
  * File editor events listener.
@@ -39,35 +36,30 @@ class FileEditorEventsListener : FileEditorManagerListener.Before {
    */
   override fun beforeFileClosed(source: FileEditorManager, file: VirtualFile) {
     val configService = service<ConfigService>()
-    val attributes = dataOpsManager.tryToGetAttributes(file)
-    if (
-      file is MFVirtualFile
-      && !configService.isAutoSyncEnabled
-      && file.isWritable
-      && attributes != null
-    ) {
-      val document = FileDocumentManager.getInstance().getDocument(file) ?: let {
-        log.info("Document cannot be used here")
-        return
-      }
-      val contentSynchronizer = service<DataOpsManager>().getContentSynchronizer(file)
-      val syncProvider = DocumentedSyncProvider(file, SaveStrategy.default(source.project))
-      val currentContent = runReadActionInEdtAndWait { syncProvider.retrieveCurrentContent() }
-      val previousContent = contentSynchronizer?.successfulContentStorage(syncProvider)
-      if (!(currentContent contentEquals previousContent)) {
-        if (showSyncOnCloseDialog(file.name, source.project)) {
-          runModalTask(
-            title = "Syncing ${file.name}",
-            project = source.project,
-            cancellable = true
-          ) {
-            runWriteActionInEdtAndWait {
-              FileDocumentManager.getInstance().saveDocument(document)
-              contentSynchronizer?.synchronizeWithRemote(syncProvider, it)
+    val syncProvider = DocumentedSyncProvider(file, SaveStrategy.default(source.project))
+    if (!configService.isAutoSyncEnabled) {
+      val attributes = dataOpsManager.tryToGetAttributes(file)
+      if (file is MFVirtualFile && file.isWritable && attributes != null) {
+        val contentSynchronizer = service<DataOpsManager>().getContentSynchronizer(file)
+        val currentContent = runReadActionInEdtAndWait { syncProvider.retrieveCurrentContent() }
+        val previousContent = contentSynchronizer?.successfulContentStorage(syncProvider)
+        if (!(currentContent contentEquals previousContent)) {
+          if (showSyncOnCloseDialog(file.name, source.project)) {
+            runModalTask(
+              title = "Syncing ${file.name}",
+              project = source.project,
+              cancellable = true
+            ) {
+              runWriteActionInEdtAndWait {
+                syncProvider.saveDocument()
+                contentSynchronizer?.synchronizeWithRemote(syncProvider, it)
+              }
             }
           }
         }
       }
+    } else {
+      syncProvider.saveDocument()
     }
     super.beforeFileClosed(source, file)
   }
