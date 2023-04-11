abe9f745292841fccf5d13de02bc50cc3cf770a0
Uladzislau <leksilonchikk@gmail.com>
2023-04-11 13:48:37 +0200

    IJMP-953 Added small fix

--- src/test/kotlin/eu/ibagroup/formainframe/utils/UtilsTestSpec.kt
+++ src/test/kotlin/eu/ibagroup/formainframe/utils/UtilsTestSpec.kt
@@ -791,8 +791,8 @@ class UtilsTestSpec : ShouldSpec({
   }
   context("utils module: EncodingUtil") {
     // saveIn
-    should("convert file content to new encoding")
+    should("convert file content to new encoding") {}
     // reloadIn
-    should("reload file content to new encoding")
+    should("reload file content to new encoding") {}
   }
 })
