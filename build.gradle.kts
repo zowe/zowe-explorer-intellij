5b6dbd6a6d24b68ea90a452c6ac9564e2f947f87
Uladzislau <leksilonchikk@gmail.com>
2024-01-22 13:30:26 +0100

    EAP prepared
    Signed-off-by: Uladzislau <leksilonchikk@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
 }
 
 intellij {
-  version.set("2023.1")
+  version.set("LATEST-EAP-SNAPSHOT")
 }
 
 tasks {
@@ -87,8 +87,8 @@ tasks {
   }
 
   patchPluginXml {
-    sinceBuild.set("231.8109")
-    untilBuild.set("233.*")
+    sinceBuild.set("241.8102")
+    untilBuild.set("241.*")
     changeNotes.set(
       """
       <b>New features:</b>
