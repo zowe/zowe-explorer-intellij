60c14575a15aab4d983824f32b14957d7aba6fe7
Dzianis Lisiankou <dlisiankou@iba.by>
2024-03-15 11:10:09 +0100

    IJMP-1537: move tasks from 2.0.0 to 1.2.0

--- src/main/kotlin/eu/ibagroup/formainframe/explorer/ui/AllocationDialog.kt
+++ src/main/kotlin/eu/ibagroup/formainframe/explorer/ui/AllocationDialog.kt
@@ -22,6 +22,7 @@ import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.builder.toNullableProperty
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import com.intellij.ui.layout.selectedValueMatches
+import eu.ibagroup.formainframe.common.message
 import eu.ibagroup.formainframe.common.ui.StatefulDialog
 import eu.ibagroup.formainframe.config.connect.ConnectionConfig
 import eu.ibagroup.formainframe.config.connect.getUsername
@@ -133,6 +134,10 @@ class AllocationDialog(project: Project?, config: ConnectionConfig, override var
           .bindItem(state.allocationParameters::allocationUnit.toNullableProperty())
           .also { spaceUnitBox = it.component }
           .widthGroup(sameWidthComboBoxGroup)
+        contextHelp(
+          description = message("allocation.dialog.unit.size.hint.description"),
+          title = message("allocation.dialog.unit.size.hint.title")
+        )
       }
       row {
         label("Primary allocation: ")
