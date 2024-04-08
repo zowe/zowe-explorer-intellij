9db42fb8a941f306bdd28d64cd26c4c6defd4e52
Arseni Tsikhamirau <atsikhamirau@ibagroup.eu>
2024-04-08 14:20:02 +0200

    IJMP-1559-Validate-owner-while-retrieving-from-TSO

--- src/main/kotlin/eu/ibagroup/formainframe/config/connect/ui/zosmf/ConnectionDialogState.kt
+++ src/main/kotlin/eu/ibagroup/formainframe/config/connect/ui/zosmf/ConnectionDialogState.kt
@@ -29,9 +29,9 @@ data class ConnectionDialogState(
   /*var apiMeditationLayer: String = "",*/
   override var username: String = "",
   override var password: String = "",
+  override var owner: String = "",
   var isAllowSsl: Boolean = false,
   var zVersion: ZVersion = ZVersion.ZOS_2_1,
-  var owner: String = "",
   override var mode: DialogMode = DialogMode.CREATE
 ) : ConnectionDialogStateBase<ConnectionConfig>() {
 
