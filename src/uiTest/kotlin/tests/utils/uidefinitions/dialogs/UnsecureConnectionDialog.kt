b69396024956551e82d544255bc3bf0e370663d1
Shyshkou_I <ishyshkou@ibagroup.eu>
2025-02-18 00:22:49 +0300

    Text and spec symbols in numeric fields

--- src/uiTest/kotlin/tests/utils/uidefinitions/dialogs/UnsecureConnectionDialog.kt
+++ src/uiTest/kotlin/tests/utils/uidefinitions/dialogs/UnsecureConnectionDialog.kt
@@ -24,11 +24,7 @@ class UnsecureConnectionDialog(val driver: Driver) {
 
   init {
     driver.ideFrame {
-<<<<<<<< HEAD:src/uiTest/kotlin/tests/utils/notification/UnsecureConnectionDialog.kt
       dialog = dialog(xpath = "//div[@accessiblename='Attempt to create an unsecured connection']")
-========
-      dialog = dialog("//div[@class='JEditorPane' and @visible_text='Attempt to create an unsecured connection']/ancestor::div[@class='MyDialog'][1]")
->>>>>>>> origin/feature/GH-282-automated-test-pipeline-run:src/uiTest/kotlin/tests/utils/uidefinitions/dialogs/UnsecureConnectionDialog.kt
     }
   }
 }
