201eb41536d3ff755fb16213cf38d44fc63b456f
Uladzislau <leksilonchikk@gmail.com>
2026-03-16 14:57:27 +0100

    Update JesExplorerPanel.kt
    Small change
    
    Signed-off-by: Uladzislau <leksilonchikk@gmail.com>

--- src/uiTest/kotlin/tests/utils/uidefinitions/JesExplorerPanel.kt
+++ src/uiTest/kotlin/tests/utils/uidefinitions/JesExplorerPanel.kt
@@ -6,11 +6,6 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Copyright Contributors to the Zowe Project.
- *
- * Contributors:
- *   Zowe Community
- *   IBA Group
- *   Uladzislau Kalesnikau
  */
 
 package tests.utils.uidefinitions
